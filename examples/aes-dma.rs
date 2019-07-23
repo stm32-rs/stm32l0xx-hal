c41415180a21bde7492d0cab3b260e07bf9436b1
Hanno Braun <hanno@braun-embedded.com>
2019-07-23 14:16:15 +0200

    Make sure buffers in AES DMA example are aligned

--- examples/aes-dma.rs
+++ examples/aes-dma.rs
@@ -10,6 +10,10 @@ extern crate panic_semihosting;
 
 use core::pin::Pin;
 
+use aligned::{
+    A4,
+    Aligned,
+};
 use cortex_m::{
     asm,
     interrupt,
@@ -42,7 +46,7 @@ fn main() -> ! {
     let key = [0x01234567, 0x89abcdef, 0x01234567, 0x89abcdef];
     let ivr = [0xfedcba98, 0x76543210, 0xfedcba98];
 
-    const DATA: [u8; 32] = [
+    const DATA: Aligned<A4, [u8; 32]> = Aligned([
         0x00, 0x11, 0x22, 0x33,
         0x44, 0x55, 0x66, 0x77,
         0x88, 0x99, 0xaa, 0xbb,
@@ -52,11 +56,11 @@ fn main() -> ! {
         0x44, 0x55, 0x66, 0x77,
         0x88, 0x99, 0xaa, 0xbb,
         0xcc, 0xdd, 0xee, 0xff,
-    ];
+    ]);
     let data = Pin::new(&DATA);
 
-    static mut ENCRYPTED: [u8; 32] = [0; 32];
-    static mut DECRYPTED: [u8; 32] = [0; 32];
+    static mut ENCRYPTED: Aligned<A4, [u8; 32]> = Aligned([0; 32]);
+    static mut DECRYPTED: Aligned<A4, [u8; 32]> = Aligned([0; 32]);
 
     // Prepare DMA buffers. This is safe, as this is the `main` function, and no
     // other functions have access to these statics.
@@ -112,8 +116,8 @@ fn main() -> ! {
         encrypted             = rx_res.buffer;
         aes                   = ctr_stream.finish();
 
-        assert_ne!(encrypted, Pin::new(&mut [0; 32]));
-        assert_ne!(encrypted, data);
+        assert_ne!(**encrypted, [0; 32]);
+        assert_ne!(**encrypted, **data);
 
         let mut ctr_stream = aes.start_ctr_stream(key, ivr);
         let mut tx_transfer = ctr_stream.tx
@@ -164,6 +168,6 @@ fn main() -> ! {
         decrypted             = rx_res.buffer;
         aes                   = ctr_stream.finish();
 
-        assert_eq!(decrypted, data);
+        assert_eq!(**decrypted, **data);
     }
 }
