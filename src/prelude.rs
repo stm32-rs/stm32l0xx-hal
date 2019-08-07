edeb3ad715cf74a41c40f631a71e95a692d8d700
Hanno Braun <hanno@braun-embedded.com>
2019-08-07 12:16:15 +0200

    Clean up prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,20 +1,29 @@
-pub use embedded_hal::digital::v2::*;
-pub use embedded_hal::prelude::*;
+pub use embedded_hal::{
+    prelude::*,
+    digital::v2::*,
+    adc::OneShot as _,
+    watchdog::{
+        Watchdog as _,
+        WatchdogEnable as _,
+    },
+};
 
-pub use crate::hal::adc::OneShot as _;
-pub use crate::hal::watchdog::Watchdog as _;
-pub use crate::hal::watchdog::WatchdogEnable as _;
-
-pub use crate::adc::AdcExt as _;
-pub use crate::delay::DelayExt as _;
-pub use crate::exti::ExtiExt as _;
-pub use crate::gpio::GpioExt as _;
-pub use crate::i2c::I2cExt as _;
-pub use crate::rcc::RccExt as _;
-pub use crate::serial::Serial1Ext as _;
-pub use crate::serial::Serial2Ext as _;
-pub use crate::spi::SpiExt as _;
-pub use crate::time::U32Ext as _;
-pub use crate::timer::TimerExt as _;
-pub use crate::watchdog::IndependedWatchdogExt as _;
-pub use crate::watchdog::WindowWatchdogExt as _;
+pub use crate::{
+    adc::AdcExt as _,
+    delay::DelayExt as _,
+    exti::ExtiExt as _,
+    gpio::GpioExt as _,
+    i2c::I2cExt as _,
+    rcc::RccExt as _,
+    serial::{
+        Serial1Ext as _,
+        Serial2Ext as _,
+    },
+    spi::SpiExt as _,
+    time::U32Ext as _,
+    timer::TimerExt as _,
+    watchdog::{
+        IndependedWatchdogExt as _,
+        WindowWatchdogExt as _,
+    },
+};
