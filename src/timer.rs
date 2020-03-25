e2ddb4b296e63bd1d869b2889c1bfcaab44b4c8d
Hanno Braun <hanno@braun-embedded.com>
2020-03-25 12:42:37 +0100

    Add support for master mode selection to `timer`

--- src/timer.rs
+++ src/timer.rs
@@ -1,6 +1,6 @@
 //! Timers
 use crate::hal::timer::{CountDown, Periodic};
-use crate::pac::{TIM2, TIM21, TIM22, TIM3};
+use crate::pac::{TIM2, TIM21, TIM22, TIM3, tim2, tim21, tim22};
 use crate::rcc::{Clocks, Rcc};
 use crate::time::Hertz;
 use cast::{u16, u32};
@@ -83,7 +83,7 @@ impl TimerExt<SYST> for SYST {
 impl Periodic for Timer<SYST> {}
 
 macro_rules! timers {
-    ($($TIM:ident: ($tim:ident, $timXen:ident, $timXrst:ident, $apbenr:ident, $apbrstr:ident, $timclk:ident),)+) => {
+    ($($TIM:ident: ($tim:ident, $timXen:ident, $timXrst:ident, $apbenr:ident, $apbrstr:ident, $timclk:ident, $mms:ty),)+) => {
         $(
             impl TimerExt<$TIM> for $TIM {
                 fn timer<T>(self, timeout: T, rcc: &mut Rcc) -> Timer<$TIM>
@@ -94,7 +94,7 @@ macro_rules! timers {
                 }
             }
 
-            impl Timer<$TIM> {
+            impl Timer<$TIM> where $TIM: GeneralPurposeTimer {
                 /// Configures a TIM peripheral as a periodic count down timer
                 pub fn $tim<T>(tim: $TIM, timeout: T, rcc: &mut Rcc) -> Self
                 where
@@ -142,6 +142,13 @@ macro_rules! timers {
                     // continue
                     self.tim.cr1.modify(|_, w| w.cen().set_bit());
                 }
+
+                /// Select master mode
+                pub fn select_master_mode(&mut self,
+                    variant: <$TIM as GeneralPurposeTimer>::MasterMode,
+                ) {
+                    self.tim.select_master_mode(variant);
+                }
             }
 
             impl CountDown for Timer<$TIM> {
@@ -188,13 +195,32 @@ macro_rules! timers {
             }
 
             impl Periodic for Timer<$TIM> {}
+
+            impl GeneralPurposeTimer for $TIM {
+                type MasterMode = $mms;
+
+                fn select_master_mode(&mut self, variant: Self::MasterMode) {
+                    self.cr2.modify(|_, w| w.mms().variant(variant));
+                }
+            }
         )+
     }
 }
 
 timers! {
-    TIM2: (tim2, tim2en, tim2rst, apb1enr, apb1rstr, apb1_tim_clk),
-    TIM3: (tim3, tim3en, tim3rst, apb1enr, apb1rstr, apb1_tim_clk),
-    TIM21: (tim21, tim21en, tim21rst, apb2enr, apb2rstr, apb2_tim_clk),
-    TIM22: (tim22, tim22en, tim22rst, apb2enr, apb2rstr, apb2_tim_clk),
+    TIM2: (tim2, tim2en, tim2rst, apb1enr, apb1rstr, apb1_tim_clk,
+        tim2::cr2::MMS_A),
+    TIM3: (tim3, tim3en, tim3rst, apb1enr, apb1rstr, apb1_tim_clk,
+        tim2::cr2::MMS_A),
+    TIM21: (tim21, tim21en, tim21rst, apb2enr, apb2rstr, apb2_tim_clk,
+        tim21::cr2::MMS_A),
+    TIM22: (tim22, tim22en, tim22rst, apb2enr, apb2rstr, apb2_tim_clk,
+        tim22::cr2::MMS_A),
+}
+
+
+pub trait GeneralPurposeTimer {
+    type MasterMode;
+
+    fn select_master_mode(&mut self, variant: Self::MasterMode);
 }
